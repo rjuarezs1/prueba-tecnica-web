da6c1a29b5cef2b555c519907492980b04a1252e
RAUL  JUAREZ <rjuarezs1@miumg.edu.gt>
2025-02-06 19:14:20 -0600

    se crearon todas las rutas para el CRUD de la table products

--- backend/src/controllers/product.controllers.js
+++ backend/src/controllers/product.controllers.js
@@ -1,6 +1,8 @@
 import { genTryCatch } from "../middlewares/genTryCatchHandler";
 import { matchedData } from "express-validator";
 import Product from "../models/product.models";
+import jwt from "jsonwebtoken";
+import { ACCESS_TOKEN } from "../config/config";
 
 export const all_products = genTryCatch(async (req, res) => {
   // se realiza el model querying de sequelize para obtener
@@ -10,6 +12,7 @@ export const all_products = genTryCatch(async (req, res) => {
       exclude: ["updatedAt"],
     },
   });
+
   res.status(200).json({
     ok: true,
     body: productos,
@@ -55,6 +58,13 @@ export const one_product = genTryCatch(async (req, res) => {
     where: { product_id: id },
   });
 
+  if (producto === null) {
+    return res.status(403).json({
+      ok: false,
+      body: producto,
+    });
+  }
+
   res.status(200).json({
     ok: true,
     body: producto,
@@ -62,11 +72,51 @@ export const one_product = genTryCatch(async (req, res) => {
 });
 
 export const update_product = genTryCatch(async (req, res) => {
+  const data = matchedData(req);
+  const product_id = data.product_id;
+  const name = data.name.toLowerCase();
+  const description = data.description.toLowerCase();
+  const price = parseFloat(data.price);
+  const stock = data.stock.toLowerCase() === "true";
+
+  const inputs = {
+    name,
+    description,
+    price,
+    stock,
+  };
+
+  const [producto_actualizado] = await Product.update(inputs, {
+    where: { product_id },
+  });
+
+  if (producto_actualizado === 0) {
+    return res.status(403).json({
+      ok: false,
+      body: `${producto_actualizado} productos actualizados`,
+    });
+  }
+
   res.status(200).json({
-    body: "PRODUCT UPDATED",
+    ok: true,
+    body: `${producto_actualizado} productos actualizados`,
   });
 });
 
 export const delete_product = genTryCatch(async (req, res) => {
+  const { product_id } = matchedData(req);
+  const id = product_id.toLowerCase();
+
+  const producto_eliminado = await Product.destroy({
+    where: { product_id: id },
+  });
+
+  if (producto_eliminado === 0) {
+    return res.status(403).json({
+      ok: false,
+      body: `${producto_eliminado} productos eliminados`,
+    });
+  }
+
   res.sendStatus(204);
 });
--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -3,23 +3,36 @@ import * as productControllers from "../controllers/product.controllers";
 import {
   createProductValidator,
   oneProductValidator,
+  updateProductValidator,
+  deleteProductValidator,
 } from "../validators/productValHandler";
 
 const router = Router();
 
 router.get("/products", productControllers.all_products);
+
 router.post(
   "/products",
   createProductValidator,
   productControllers.create_product
 );
+
 router.get(
   "/products/:product_id",
   oneProductValidator,
   productControllers.one_product
 );
 
-router.put("/products/:product_id", productControllers.update_product);
-router.delete("/products/:product_id", productControllers.delete_product);
+router.put(
+  "/products/:product_id",
+  updateProductValidator,
+  productControllers.update_product
+);
+
+router.delete(
+  "/products/:product_id",
+  deleteProductValidator,
+  productControllers.delete_product
+);
 
 export default router;
